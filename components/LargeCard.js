2b6bcd215a44cbce7cdfbb66376e84e82c41428b
Manav Khadka <manavkhadka0@gmail.com>
2021-08-05 09:48:33 +0545

    Carousel added

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -8,8 +8,8 @@ function LargeCard({ img, title, description, buttonText }) {
       </div>
 
       <div className="absolute top-32 left-12">
-        <h3 className="text-4xl mb-3 w-64">{title}</h3>
-        <p>{description}</p>
+        <h3 className="text-4xl mb-3 w-64 text-black">{title}</h3>
+        <p className="text-black">{description}</p>
         <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
           {buttonText}
         </button>
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,53 @@ import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 import Footer from "../components/Footer";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Home({ exploreData, cardsData }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  const settings2 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="">
       <Head>
@@ -19,10 +64,11 @@ export default function Home({ exploreData, cardsData }) {
       {/* Banner */}
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 bg-gray-900 text-white">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           {/* Pull data from a server - API */}
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
@@ -38,18 +84,42 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
-          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3">
-            {cardsData?.map(({ img, title }) => (
-              <MediumCard key={img} img={img} title={title} />
-            ))}
+          <div className="">
+            <Slider {...settings2}>
+              {cardsData?.map(({ img, title }) => (
+                <div className="">
+                  <MediumCard key={img} img={img} title={title} />
+                </div>
+              ))}
+            </Slider>
           </div>
         </section>
-        <LargeCard
-          img="https://links.papareact.com/4cj"
-          title="The Greatest Outdoors"
-          description="Wishlists curated by Airbnb"
-          buttonText="Get Inspired"
-        />
+        <Slider {...settings}>
+          <div className="">
+            <LargeCard
+              img="https://links.papareact.com/4cj"
+              title="The Greatest Outdoors"
+              description="Wishlists curated by Airbnb"
+              buttonText="Get Inspired"
+            />
+          </div>
+          <div className="">
+            <LargeCard
+              img="https://links.papareact.com/0fm"
+              title="The Greatest Outdoors"
+              description="Wishlists curated by Airbnb"
+              buttonText="Get Inspired"
+            />
+          </div>
+          <div className="">
+            <LargeCard
+              img="https://links.papareact.com/4cj"
+              title="The Greatest Outdoors"
+              description="Wishlists curated by Airbnb"
+              buttonText="Get Inspired"
+            />
+          </div>
+        </Slider>
       </main>
       <Footer />
     </div>
